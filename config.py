95402ae86001f6191a7002fc949d3e3474c13e05
Zayus13 <sven_loechte@protonmail.com>
2022-04-18 19:58:21 +0200

    fixed naming

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ def config(section, filename='logins.ini'):
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
-            db[param[0]] = param[1]
+            configs[param[0]] = param[1]
     else:
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
