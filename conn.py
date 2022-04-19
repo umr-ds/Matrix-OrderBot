ff465107fbbbfe407eb2a0b803026275fdf0a73f
Zayus13 <sven_loechte@protonmail.com>
2022-04-19 22:53:06 +0200

    trying Bot as class

--- conn.py
+++ conn.py
@@ -1,19 +1,41 @@
-from nio import AsyncClient, RoomMessageText
 import asyncio
+from pprint import pprint
 
-import config
+from nio import AsyncClient, RoomMessageText
+from psycopg2 import connect
+
+from config import config
 
 
-async def message_cb(room, event):
-    print(room.display_name, room.user_name(event.sender), event.body)
+class Bot:
+
+    def __init__(self):
+        self.client = None
+        self.conn = None
+
+    async def connect(self):
+        conf = config('matrix')
+        try:
+            self.conn = connect(**config('postgresql'))
+            self.client = AsyncClient(conf['server'], conf['username'])
+            print(await self.client.login(conf['password']))
+        except Exception as error:
+            print(error)
+        finally:
+            self.conn.close()
+
+        self.client.add_event_callback(self.message_cb, RoomMessageText)
+        await self.client.sync_forever(0)
+
+        async def message_cb(room, event):
+            # todo: extract message + name/username + give to parser
+            pprint(vars(room))
+            pprint(vars(event))
 
 
 async def main():
-    conf = config.config('matrix')
-    client = AsyncClient(conf['server'], conf['username'])
-    client.add_event_callback(message_cb, RoomMessageText)
-    print(await client.login(conf['password']))
-    await client.sync_forever(0)
+    bot = Bot()
+    await bot.connect()
 
 
 if __name__ == '__main__':
