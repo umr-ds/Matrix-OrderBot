e0a659821cb1c03ac08780e5bcbdb8c8789c2374
Zayus13 <sven_loechte@protonmail.com>
2022-06-08 19:52:35 +0200

    undid db types changes in commit 5c06e9f6

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,22 +3,22 @@ CREATE TABLE participant
     id         SERIAL PRIMARY KEY,
     name       VARCHAR NOT NULL,
     username   VARCHAR,
-    user_total MONEY DEFAULT 0
+    user_total NUMERIC DEFAULT 0
 );
 CREATE TABLE orders
 (
     order_id  SERIAL PRIMARY KEY,
     name      VARCHAR,
-    total     MONEY CHECK (total >= 0),
-    price     MONEY CHECK (price >= 0),
-    tip       MONEY CHECK (tip >= 0),
-    timestamp DATE
-
+    total     NUMERIC CHECK (total >= 0),
+    price     NUMERIC CHECK (price >= 0),
+    tip       NUMERIC CHECK (tip >= 0),
+    timestamp DATE,
+    CHECK (total = price + tip)
 );
 CREATE TABLE cuts
 (
     order_id  INT REFERENCES orders (order_id),
     id        INT REFERENCES participant (id),
-    cut       MONEY NOT NULL,
+    cut       NUMERIC NOT NULL,
     timestamp DATE
 );
