5adcfb47e60786d7513d5104efab784d60a01071
Zayus13 <sven_loechte@protonmail.com>
2022-12-07 16:51:40 +0100

    enable multipay

--- orderbotapp/order.py
+++ orderbotapp/order.py
@@ -1,9 +1,10 @@
-from typing import Union, Tuple, List
-
+import logging
+from typing import Union, Tuple, List, Dict
 
 from db_classes import DB_Order
 
 order_version: int = 1
+
 
 class Order:
 
@@ -13,9 +14,10 @@ class Order:
         self.name: str = name
         self.price: int = 0
         self.tip: int = 0
-        self.paid: Union[str, bool] = True
+        self.paid: bool = False
         self.recommended_payer: Tuple[str, int] = None
         self.version: int = 1
+        self.payers: Dict[str, int] = {}
 
     def __str__(self):
         return self.print_order()
@@ -43,13 +45,25 @@ class Order:
                 else:
                     order += f"{'':<{maxName + 2}}{item[0]:<{maxItem}} {util.cent_to_euro(item[1]):>{maxAmount}}"
                 order += "\n"
+
+        recommadation = f"(Recommended Payer: {self.recommended_payer[0].title() if self.recommended_payer else 'None'} [{self.recommended_payer[1] if self.recommended_payer else '0'}])"
+        if self.paid is True:
+            paid_string = f"Paid by {', '.join([f'{user.title()} [{util.cent_to_euro(self.payers[user])}]' for user in self.payers])}"
+        elif self.paid is False and len(self.payers) > 0:
+            paid_string = f"Partially paid by {', '.join([f'{user.title()} [{util.cent_to_euro(self.payers[user])}]' for user in self.payers])}"
+            paid_string += f" (Missing {util.cent_to_euro(self.price + self.tip - self.sum_payed())})"
+        else:
+            paid_string = "Not paid yet"
+
         s = (title, order,
-            f"{'Tip:':<{maxName + 2}}{'':<{maxItem}} {util.cent_to_euro(self.tip):>{maxAmount}}",
-            f"{'Total:':<{maxName + 2}}{'':<{maxItem}} {util.cent_to_euro(self.price):>{maxAmount}}",
-            f"{'Sum:':<{maxName + 2}}{'':<{maxItem}} {util.cent_to_euro((self.tip + self.price)):>{maxAmount}}",
-            f"Paid by: {self.paid.title() if self.paid else 'Not paid'} (Recommended Payer: {self.recommended_payer[0].title() if self.recommended_payer else 'None'} [{self.recommended_payer[1] if self.recommended_payer else '0'}])"
-            )
+             f"{'Tip:':<{maxName + 2}}{'':<{maxItem}} {util.cent_to_euro(self.tip):>{maxAmount}}",
+             f"{'Total:':<{maxName + 2}}{'':<{maxItem}} {util.cent_to_euro(self.price):>{maxAmount}}",
+             f"{'Sum:':<{maxName + 2}}{'':<{maxItem}} {util.cent_to_euro((self.tip + self.price)):>{maxAmount}}",
+             recommadation,
+             paid_string)
+
         return "\n".join(s)
+
 
     def add_pos(self, user: str, item: str, amount: int) -> None:
         if user in self.order:
@@ -57,7 +71,8 @@ class Order:
         else:
             self.order[user] = [(item, amount)]
         self.price = self.price + amount
-        self.paid = None
+        self.paid = False
+
 
     def remove(self, user: str, item: str = None) -> None:
         if user.lower() not in self.order:
@@ -66,6 +81,7 @@ class Order:
             for entry in self.order[user]:
                 self.price = self.price - entry[1]
             del self.order[user]
+
         else:
             for c, entry in enumerate(self.order[user]):
                 if entry[0].lower() == item.lower():
@@ -73,30 +89,45 @@ class Order:
                     self.order[user].remove(entry)
             if len(self.order[user]) == 0:
                 del self.order[user]
+        logging.debug(self.price + self.paid)
+        if self.price <= sum(self.payers.values()):
+            self.paid = True
+
 
     def add_tip(self, tip: int) -> None:
         if tip >= 0:
             self.tip = self.tip + tip
 
+
     def remove_tip(self):
         self.tip = 0
+
 
     def sum_order(self, user: str) -> int:
         if user in self.order:
             return sum([item[0] for item in self.order[user]])
 
+
     def pay(self, user: str, amount: int = None) -> None:
         if user not in self.order:
             self.order[user] = []
         if amount is None:
-            self.order[user].append(("paid amount", -(self.price + self.tip)))
-            self.paid = user
+            self.payers[user] = self.price + self.tip - self.sum_payed()
+            self.paid = True
         else:
-            self.order[user].append(("paid amount", -amount))
+            self.payers[user] = amount
+            if self.price <= sum(self.payers.values()):
+                self.paid = True
+                self.tip = - self.price + sum(self.payers.values())
+
 
     def to_dborder(self) -> DB_Order:
         return DB_Order(name=self.name, total=self.price + self.tip, price=self.price,
                         tip=self.tip)
 
+
     def set_recommended_payer(self, user: str, amount: int) -> None:
         self.recommended_payer = (user, amount)
+
+    def sum_payed(self) -> int:
+        return sum(self.payers.values())
